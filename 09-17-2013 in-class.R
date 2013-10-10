45446af3e5dd83b375f98385002f07e25c6ae36b
Jess <jessica.stone428@gmail.com>
2013-10-10 09:54:44 -0500

    The rest of the files moved into the repository

---
+++ 09-17-2013 in-class.R
@@ -0,0 +1,43 @@
+function(x,y,r,c='red')
+circle(x=60, y=70,r=10, border=NULL, col="orange")
+
+help(polygon)
+#No
+1:10>3
+1:20>10
+1:100>75
+1:10>c(3,7)
+1:10>c(4,6)
+1:10>c(2,8)
+1:10<c(3,7)
+c(1,15,7,9)
+v = c(1,15,7,9)
+c(v,v)
+length(v)
+length(NULL)
+c(NULL,v)
+c()
+c(1:10>c(3,7))
+NULL==NULL
+thing=NULL==NULL
+5*thing
+class(thing)
+length(thing)
+class(c())
+v[c(2,2,1,3,4,1)]
+v[v>5]
+v[v>=5 & v<10]
+v[c(1,4)]<-0
+5+v
+5*v
+sum(5:10)
+prod(1:4)
+mean(4:6)
+median(1:8)
+min(1:10)
+max(3:10)
+paste(c("yes", "no", "yes"),collapse=" I THINK ")
+#single character string with the contents (yes, no, yes)
+
+NO=NULL
+5+NO
